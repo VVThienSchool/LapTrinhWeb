8424b0b6584d8dd5c8da8fe08bd81feabb5e2a30
VuVanThien <ThienVuVan2003@gmail.com>
2023-09-27 14:42:35 +0700

    practice 27-09

--- MyWebApp/Controllers/StudentController.cs
+++ MyWebApp/Controllers/StudentController.cs
@@ -16,25 +16,25 @@ namespace MyWebApp.Controllers
             {
                 Id = 101, Name = "Hải Nam", Branch = Branch.IT,
                 Gender = Gender.Male, IsRegular = true,
-                Address = "A1-2018", Email = "nam@g.com"
+                Address = "A1-2018", Email = "nam@g.com", Score = 9.8
             },
             new Student()
             {
                 Id = 102, Name = "Minh Tú", Branch = Branch.BE,
                 Gender = Gender.Female, IsRegular = true,
-                Address = "A1-2019", Email = "tu@g.com"
+                Address = "A1-2019", Email = "tu@g.com",  Score = 9.8
             },
             new Student()
             {
                 Id = 103, Name = "Hoàng phong", Branch = Branch.CE,
                 Gender = Gender.Male, IsRegular = false,
-                Address = "A1-2020", Email = "phong@g.com"
+                Address = "A1-2020", Email = "phong@g.com",  Score = 9.8
             },
             new Student()
             {
                 Id = 104, Name = "Xuân Mai", Branch = Branch.CE,
                 Gender = Gender.Female, IsRegular = false,
-                Address = "A1-2021", Email = "mai@g.com"
+                Address = "A1-2021", Email = "mai@g.com",  Score = 9.8
             }
         };
         }
@@ -67,10 +67,26 @@ namespace MyWebApp.Controllers
         [Route("Admin/Student/Add")]
         public async Task<ActionResult> Create(Student s)
         {
-            s.Id = listStudents.Last<Student>().Id + 1;
-            listStudents.Add(s);
+            //s.Id = listStudents.Last<Student>().Id + 1;
+            //listStudents.Add(s);
 
-            return View("Index", listStudents);
+            //return View("Index", listStudents);
+
+            if (ModelState.IsValid)
+            {
+                s.Id = listStudents.Last<Student>().Id + 1;
+                listStudents.Add(s);
+                return View("Index", listStudents);
+            }
+            ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            ViewBag.AllBranches = new List<SelectListItem>()
+                 {
+                 new SelectListItem { Text = "IT", Value = "1" },
+                 new SelectListItem { Text = "BE", Value = "2" },
+                 new SelectListItem { Text = "CE", Value = "3" },
+                 new SelectListItem { Text = "EE", Value = "4" }
+                };
+            return View();
         }
     }
 }
--- MyWebApp/Models/Student.cs
+++ MyWebApp/Models/Student.cs
@@ -1,15 +1,66 @@
-﻿namespace MyWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWebApp.Models
 {
     public class Student
     {
+        //public int Id { get; set; }
+        //[Required]
+        //public string? Name { get; set; }
+        //[Required(ErrorMessage = "Email bắt buộc phải được nhập")]
+        //[RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
+        //public string? Email { get; set; }
+        //[StringLength(100, MinimumLength = 8)]
+        //[Required]
+        //public string? Password { get; set; }
+        //[Required]
+        //public Branch? Branch { get; set; }
+        //[Required]
+        //public Gender? Gender { get; set; }
+        //public bool IsRegular { get; set; }
+        //[DataType(DataType.MultilineText)]
+        //[Required]
+        //public string? Address { get; set; }
+        //[Range(typeof(DateTime), "1/1/1963", "12/31/2005")]
+        //[DataType(DataType.Date)]
+        //[Required]
+        //public DateTime DateOfBirth { get; set; }
+
+
+
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Hãy nhập tên.")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Tên phải có ít nhất 4 và tối đa 100 ký tự.")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Hãy nhập địa chỉ email.")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@gmail\.com", ErrorMessage = "Địa chỉ email phải có đuôi gmail.com.")]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Hãy nhập mật khẩu.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt.")]
         public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Hãy chọn chi nhánh.")]
         public Branch? Branch { get; set; }
+
+        [Required(ErrorMessage = "Hãy chọn giới tính.")]
         public Gender? Gender { get; set; }
+
         public bool IsRegular { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Hãy nhập địa chỉ.")]
         public string? Address { get; set; }
+
+        [Range(typeof(DateTime), "1/1/1963", "12/31/2005", ErrorMessage = "Ngày sinh phải nằm trong khoảng từ 1/1/1963 đến 31/12/2005.")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Hãy nhập ngày sinh.")]
         public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Hãy nhập điểm.")]
+        [Range(0.0, 10.0, ErrorMessage = "Điểm phải nằm trong khoảng từ 0.0 đến 10.0.")]
+        public double? Score { get; set; }
     }
 }
